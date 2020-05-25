7ae1499782f5ff14a08dbc1edfe9c0c55d87de0d
Amy Gori <amydgori@gmail.com>
2020-05-25 18:08:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Hello, World!
+## JS Edition
+
+The main.js file contains examples of expressions that you can output to the console using `console.log`.
+
+To get started, open index.html in the browser, open the dev tools and click on the console tab.
+
+In your code editor, open `main.js` and work on the exercises in there. You'll need to uncomment the necessary lines as you work your way through each numbered exercise and run each one.
---
+++ main.js
@@ -0,0 +1,43 @@
+// Intro JS exercise
+console.log('oh hai! Welcome to the console!')
+console.log('You can run JavaScript directly here.')
+console.log('You can also log any output from your JavaScript files.')
+
+// Read carefully and uncomment the necessary lines one at a time as you run the code.
+
+// 1. What does `firstName` evaluate to on this line? Console.log to find out.
+let firstName = "Jamal"
+// write your console.log here
+
+// 2. What would you say what is happening here?
+// what does `firstName` evaluate to now?
+// Uncomment the following lines and see what it is with console.log:
+// firstName = "Avery"
+// console.log("The firstName variable now has an assigned value of ", firstName)
+
+// 3. What is happening here? Use console.log() to check if you're right.
+// Uncomment the following line:
+// let name = firstName
+// console.log("The name variable evaluates to ", name)
+
+// 4. Assign a value to the `greeting` variable that is a string that represents a greeting, and log it to the console.
+let greeting
+
+// 5. Combine the `greeting` and `name` variables to output a personalized greeting. Log it to the console.
+// Write your code here
+
+// 6. What does `result` evaluate to?
+// Uncomment the following lines and use console.log() to check what value is store in the variable `result`
+// let num1 = 50
+// let num2  = 10
+// let result = num1 - num2
+
+// 7. What will `result` return here? Log it to the console to find out.
+// Uncomment the following line:
+// result = num1 - "5"
+
+// 8. Create a variable to calculate the total number of tacos and burritos.
+// Log a sentence that tells the user the total number.
+// Uncomment the following lines and then write your code:
+// let tacos = 8
+// let burritos = 4
