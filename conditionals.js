9513be9d9c1514a1deeeb1b637305944555c6783
Amy Gori <amydgori@gmail.com>
2023-10-05 17:15:32 -0400

    Update

--- conditionals.js
+++ conditionals.js
@@ -1,8 +1,24 @@
-
 /*
 Conditionals: using if/else statements
-Uncomment the necessary lines one at a time as you run the code.
+Read and predict what you think will happen when the code runs, and check the console to see the output.
 */
+
+let dogsAreCool = true
+let catsAreMean = false
+
+if (dogsAreCool) {
+  console.log('Dogs are in fact very cool.')
+} else {
+  console.log('Dogs are not cool.')
+}
+
+if (catsAreMean) {
+  console.log('Told ya, cats are mean.')
+} else {
+  console.log('Cats are lovely creatures of fluff.')
+}
+
+// Uncomment the necessary lines one at a time to run the code examples below.
 
 // 1. What will be logged to the console when the statement below runs?
 // let x;
@@ -13,14 +29,12 @@ Uncomment the necessary lines one at a time as you run the code.
 //   console.log("False!");
 // }
 
-
 // 2. Which branch of the if/else statement will run here?
 // if ("") {
 //   console.log("True!");
 // } else {
 //   console.log("False!");
 // }
-
 
 // 3.
 // if (-5 && 5) {
@@ -29,14 +43,12 @@ Uncomment the necessary lines one at a time as you run the code.
 //   console.log("False!");
 // }
 
-
 // 4.
 // if ("Hello World!") {
 //   console.log("True!");
 // } else {
 //   console.log("False!");
 // }
-
 
 // 5. Try running this a few times, changing the boolean values for a and b
 // so that the logic will run with different combinations. Observe the outcomes.
--- main.js
+++ main.js
@@ -1,43 +1,52 @@
 // Intro JS exercise
 console.log('oh hai! Welcome to the console!')
-console.log('You can run JavaScript directly here.')
-console.log('You can also log any output from your JavaScript files.')
+console.log(
+  "You can write and run JavaScript directly here. It's like a scratchpad for your code."
+)
+console.log('You can also log any output from your JavaScript files here.')
 
 // Read carefully and uncomment the necessary lines one at a time as you run the code.
 
-// 1. What does `firstName` evaluate to on this line? Console.log to find out.
-let firstName = "Jamal"
-// write your console.log here
+// 1. What does `firstName` evaluate to on the following lines?
+// Uncomment the following lines and reload the page in the browser to find out.
+// let firstName = 'Jamal'
+// console.log('The firstName variable evaluates to ', firstName)
 
-// 2. What would you say what is happening here?
-// what does `firstName` evaluate to now?
-// Uncomment the following lines and see what it is with console.log:
+// 2. We can reassign values to existing variables. Here we are reassigning the a new string to the `firstName` variable.
+// Uncomment the following lines to run the code, and look in the console to see the output.
 // firstName = "Avery"
 // console.log("The firstName variable now has an assigned value of ", firstName)
 
-// 3. What is happening here? Use console.log() to check if you're right.
-// Uncomment the following line:
+// 3. We can assign the value of one variable to another variable.
+// Uncomment the following lines to run the code, and look in the console to see the output:
 // let name = firstName
 // console.log("The name variable evaluates to ", name)
 
-// 4. Assign a value to the `greeting` variable that is a string that represents a greeting, and log it to the console.
-let greeting
+// 4. Now you try assigning a value to a variable.
+// Assign a value to the `greeting` variable that is a string. It could be anything, like "Hi" or "What's up" or "Guten tag".
+// Then, write another line for your console.log statement that will log the variable to the console.
+// let greeting
 
-// 5. Combine the `greeting` and `name` variables to output a personalized greeting. Log it to the console.
-// Write your code here
+// 5. We can combine the `greeting` and `name` variables to output a personalized greeting. Uncomment and check the console.
+// console.log(greeting + ", " + name + "!")
 
-// 6. What does `result` evaluate to?
-// Uncomment the following lines and use console.log() to check what value is store in the variable `result`
-// let num1 = 50
-// let num2  = 10
-// let result = num1 - num2
+// 6. We can do math with variables, too. What does `result` evaluate to in the following lines?
+// Read the following code and make a prediction about what you think the value of `result` will be.
+// Uncomment the following lines and check the console to verify what value is store in the variable `result`
+// let score1 = 50
+// let score2  = 10
+// let result = score1 - score2
+// console.log("Result is:", result)
 
-// 7. What will `result` return here? Log it to the console to find out.
-// Uncomment the following line:
-// result = num1 - "5"
+// 7. JavaScript will try to convert strings to numbers when doing math.
+// Predict what `result` will return here. Then uncomment the following lines and check the console to verify.
+
+// result = score1 - "5"
 
 // 8. Create a variable to calculate the total number of tacos and burritos.
 // Log a sentence that tells the user the total number.
 // Uncomment the following lines and then write your code:
 // let tacos = 8
 // let burritos = 4
+// write your code here
+// don't forget to log to the console
